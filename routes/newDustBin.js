0f776a2b7d6c650530128117cf9c887f0b8a5683
Shrishail Zalake <shrishailzalake@gmail.com>
2018-05-21 12:03:36 +0530

    database Update corrected

--- routes/newDustBin.js
+++ routes/newDustBin.js
@@ -7,14 +7,38 @@ router.get('/',function(req,res){
 	console.log(req.query.lat);
 	console.log(req.query.lng);
 
-
-	var newDustBin = new dustBin({
-		id: req.query.id,
-		long: req.query.lng,
-		lat: req.query.lat,
-		status: "full"
+	dustBin.findOne({'id':req.query.id},function(err,result){
+		if(result==null){
+			var newDustBin = new dustBin({
+			id: req.query.id,
+			long: req.query.lng,
+			lat: req.query.lat,
+			status: "full"
+			});
+			newDustBin.save(callBack);
+		}
+		else{
+			dustBin.update({'id':req.query.id},{$set:{'long':req.query.lng,'lat':req.query.lat}},function(err,result){
+				console.log(err);
+				dustBin.find(function(err,result){
+					var locations=[];
+					for(i=0;i<result.length;i++)
+					{	
+						locations.push(result[i].id);
+						locations.push(result[i].lat);
+						locations.push(result[i].long);
+						locations.push(result[i].status);
+				
+					}
+					req.io.emit('newDustBin',locations);
+				});
+			console.log('new data sent');
+			res.redirect('/');
+			});
+		}
 	});
-	newDustBin.save(callBack);
+	
+	
 	
 	function callBack(){
 
